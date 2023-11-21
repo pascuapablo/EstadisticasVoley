dd8212ffd509ba1ee9bfc04e18edf90a6d531884
ppascua <pablo.pascua@mercadolibre.com>
2023-11-21 11:20:33 -0300

    minor fixes

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import PlayerList from "./PlayerList";
 import StatsInput from "./StatsInput";
 import StatsResults from "./StatsResults";
 import {Row, Tab, Tabs} from "react-bootstrap";
+
 function App() {
     const [players, setPlayers] = useState([
-        {name: 'Mario', position: 'Punta', isInCourt: false, number:1},
-        {name: 'Pablo', position: 'Central', isInCourt: false, number:12},
-        {name: 'Elias', position: 'Opuesto', isInCourt: false, number:13},
-        {name: 'Maxi', position: 'Libero', isInCourt: false, number:41},
-        {name: 'Santi', position: 'Central', isInCourt: false, number:5},
-        {name: 'Lauro', position: 'Central', isInCourt: false, number:6},
-        {name: 'Fede', position: 'Armador', isInCourt: false, number:7},
-        {name: 'Negro', position: 'Armador', isInCourt: false, number:8},
-        {name: 'Facu', position: 'Punta', isInCourt: false, number:9},
-        {name: 'Sebas', position: 'Punta', isInCourt: false, number:11},
+        {name: 'Mario', position: 'Punta', isInCourt: false, number: 1},
+        {name: 'Pablo', position: 'Central', isInCourt: false, number: 12},
+        {name: 'Elias', position: 'Opuesto', isInCourt: false, number: 13},
+        {name: 'Maxi', position: 'Libero', isInCourt: false, number: 41},
+        {name: 'Santi', position: 'Central', isInCourt: false, number: 5},
+        {name: 'Lauro', position: 'Central', isInCourt: false, number: 6},
+        {name: 'Fede', position: 'Armador', isInCourt: false, number: 7},
+        {name: 'Negro', position: 'Armador', isInCourt: false, number: 8},
+        {name: 'Facu', position: 'Punta', isInCourt: false, number: 9},
+        {name: 'Sebas', position: 'Punta', isInCourt: false, number: 11},
     ]);
 
     const [stats, setStats] = useState([]);
 
-    const addPlayer = (name, position) => {
-        const newPlayer = {name, position, isInCancha: false};
+    const addPlayer = (name, position, playerNumber) => {
+        const newPlayer = {name, position, isInCancha: false, number: playerNumber};
         setPlayers([...players, newPlayer]);
     }
 
@@ -45,7 +46,7 @@ function App() {
                 id="uncontrolled-tab-example"
                 className="mb-4 text-black"
             >
-                <Tab eventKey="tab1" title="Jugadores " >
+                <Tab eventKey="tab1" title="Jugadores ">
                     <Row>
                         <PlayerList players={players} onToggleInCancha={toggleInCancha}/>
                     </Row>
@@ -55,7 +56,7 @@ function App() {
 
 
                 </Tab>
-                <Tab eventKey="tab2" title="Estadisticas" >
+                <Tab eventKey="tab2" title="Estadisticas">
                     <StatsInput players={players} onAddStats={addStats}/>
                 </Tab>
                 <Tab eventKey="tab3" title="Resultados" className={"text-black"}>
--- src/PlayerInput.js
+++ src/PlayerInput.js
@@ -1,16 +1,30 @@
 import React, {useState} from 'react';
 import {Button, Form, Modal, Row} from "react-bootstrap";
+import * as PropTypes from "prop-types";
+
+function Mo(props) {
+    return null;
+}
+
+Mo.propTypes = {
+    show: PropTypes.bool,
+    onHide: PropTypes.func,
+    children: PropTypes.node
+};
 
 function PlayerInput({onAddPlayer}) {
     const [playerName, setPlayerName] = useState('');
+    const [playerNumber, setPlayerNumber] = useState('');
     const [playerPosition, setPlayerPosition] = useState('');
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const handleSubmit=(e) => {
+    const handleSubmit = (e) => {
         console.log(e)
+        console.log(playerNumber, playerName, playerPosition)
+        onAddPlayer(playerName, playerPosition,playerNumber)
 
         handleClose()
     }
@@ -27,25 +41,28 @@ function PlayerInput({onAddPlayer}) {
                 <Button className={"btn btn-primary col-sm-6 text-light"} onClick={handleShow}>Agregar Jugador</Button>
                 {/*<Button className={"btn btn-primary col-sm-6"} onClick={addPlayer}>Agregar Jugador</Button>*/}
             </Row>
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={show} onHide={handleClose} fullscreen={true}>
                 <Modal.Header closeButton>
                     <Modal.Title>Agregar jugador</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
-                        <Form.Group className="mb-3" >
-                            <Form.Control type="text" placeholder="Nombre"  />
+                        <Form.Group className="mb-3">
+                            <Form.Control type="text" placeholder="Nombre" value={playerName}
+                                          onChange={e => setPlayerName(e.target.value)}/>
                         </Form.Group>
-                        <Form.Select className={"mb-3"} >
-                            <option >Armador</option>
-                            <option >Punta</option>
-                            <option >Central</option>
-                            <option >Opuesto</option>
-                            <option >Libero</option>
+                        <Form.Select className={"mb-3"} value={playerPosition}
+                                     onChange={e => setPlayerPosition(e.target.value)}>
+                            <option>Armador</option>
+                            <option>Punta</option>
+                            <option>Central</option>
+                            <option>Opuesto</option>
+                            <option>Libero</option>
 
                         </Form.Select>
                         <Form.Group className="mb-3">
-                            <Form.Control type="number" placeholder="Camiseta" />
+                            <Form.Control type="number" placeholder="Camiseta" value={playerNumber}
+                                          onChange={e => setPlayerNumber(e.target.value)}/>
                         </Form.Group>
                     </Form>
 
